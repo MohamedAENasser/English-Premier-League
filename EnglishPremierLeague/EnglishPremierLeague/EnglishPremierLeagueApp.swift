089766c2fc527cb188362341e63e4c3089ded892
Mohamed Abdelnasser <mohammed_abdelnasser@yahoo.com>
2023-03-21 22:53:18 +0200

    Initial commit

---
+++ EnglishPremierLeague/EnglishPremierLeague/EnglishPremierLeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishPremierLeagueApp.swift
+//  EnglishPremierLeague
+//
+//  Created by Mohamed Abd ElNasser on 21/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct EnglishPremierLeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
