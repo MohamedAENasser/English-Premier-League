650bd5a9e9a3d6c4239f6d1ac2ff3f218fe9265c
Mohamed Abdelnasser <mohammed_abdelnasser@yahoo.com>
2023-03-27 23:11:42 +0200

    Update error view background color

--- EnglishPremierLeague/EnglishPremierLeague/Views/StateViews/ErrorView.swift
+++ EnglishPremierLeague/EnglishPremierLeague/Views/StateViews/ErrorView.swift
@@ -12,26 +12,30 @@ struct ErrorView: View {
     var onTapReTry: () -> Void
 
     var body: some View {
-        VStack {
-            Image("Warning-icon")
-                .resizable()
-                .frame(width: 150, height: 150)
-                .padding(.bottom, 50)
+        ZStack {
+            Color.backgroundColor.ignoresSafeArea(.all)
 
-            Text(error.description)
-                .font(.title)
-                .multilineTextAlignment(.center)
+            VStack {
+                Image("Warning-icon")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .padding(.bottom, 50)
 
-            Button {
-                onTapReTry()
-            } label: {
-                Label("Try Again", systemImage: "arrow.counterclockwise")
-                    .labelStyle(.iconOnly)
+                Text(error.description)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+
+                Button {
+                    onTapReTry()
+                } label: {
+                    Label("Try Again", systemImage: "arrow.counterclockwise")
+                        .labelStyle(.iconOnly)
+                }
+                .buttonStyle(.borderless)
+                .foregroundColor(.defaultTextColor)
+                .scaleEffect(3)
+                .padding(.top, 20)
             }
-            .buttonStyle(.borderless)
-            .foregroundColor(.defaultTextColor)
-            .scaleEffect(3)
-            .padding(.top, 20)
         }
     }
 }
